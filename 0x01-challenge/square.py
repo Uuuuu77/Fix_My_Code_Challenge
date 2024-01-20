e325162ff961dee439bb62dd070b86cf7dcd9904
John Njuguna <johnmnjuguna89@gmail.com>
2024-01-20 15:28:56 +0000

    My square

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+class Square():
+    side = 0
+
+    def __init__(self, *args, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.side * self.side
+
+    def perimeter_of_my_square(self):
+        return self.side * 4
+
+    def __str__(self):
+        return "Square with side length {}".format(self.side)
+
+if __name__ == "__main__":
+    s = Square(side=12)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.perimeter_of_my_square())
